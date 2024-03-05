8ea538f5ce5890c7b58cd58eb41167fc8f738780
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-03-05 08:43:37 +0330

    implement permission enum

---
+++ src/main/java/rastak/train/ws/model/enums/Permission.java
@@ -0,0 +1,19 @@
+package rastak.train.ws.model.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Permission {
+
+    ADMIN_READ("admin:read"),
+    ADMIN_UPDATE("admin:update"),
+    ADMIN_CREATE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
+    USER_READ("user:read");
+
+
+    @Getter
+    private final String permission;
+
+}
--- src/main/java/rastak/train/ws/model/enums/UserRole.java
+++ src/main/java/rastak/train/ws/model/enums/UserRole.java
@@ -1,6 +1,49 @@
 package rastak.train.ws.model.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.var;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static rastak.train.ws.model.enums.Permission.*;
+@RequiredArgsConstructor
 public enum UserRole {
-    ADMIN,
-    USER
+
+    ROLES(Collections.emptySet()),
+
+    ADMIN(
+            Set.of(
+            ADMIN_READ,
+            ADMIN_UPDATE,
+            ADMIN_DELETE,
+            ADMIN_CREATE,
+            USER_READ
+            )
+    ),
+
+    USER(Set.of(
+            USER_READ
+    ));
+
+    @Getter
+    private final Set<Permission> permissions;
+
+    public List<SimpleGrantedAuthority> getAuthorized(){
+        var authorized = getPermissions()
+                .stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toList());
+        authorized.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+        return authorized;
+    }
 }
+
+
+
+
+
