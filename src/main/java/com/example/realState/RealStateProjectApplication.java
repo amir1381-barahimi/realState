cf759af6d67162d0fffe0404d3deb2af7313afb1
amir1381-barahimi <amirmohsenbarahimi1381@gmail.com>
2024-08-01 11:23:37 +0330

    first commit: project start

---
+++ src/main/java/com/example/realState/RealStateProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.realState;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealStateProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealStateProjectApplication.class, args);
+	}
+
+}
